5536fc2c936912629a581e76f42471df544192f1
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-01-18 13:58:19 -0300

    adding navbar fixed

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from '@/Components/NavBar'
 
 const Header = () => {
   return (
-    <header className='fixed top-0 flex w-full flex-row  justify-end shadow-2xl dark:shadow-2xl bg-white  md:px-20 md:dark:bg-slate-800 lg:px-28'>
+    <header className='fixed top-0 flex w-full flex-row  justify-end bg-white shadow-2xl dark:bg-slate-900   dark:shadow-2xl md:px-20 lg:px-28'>
       <NavBar />
     </header>
   )
